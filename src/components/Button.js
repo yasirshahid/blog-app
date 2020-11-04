218ad924b89289caf01540845ee83bef0c1281b8
yasirshahid <yasirshahid5@gmail.com>
2020-11-04 16:49:05 +0500

    remove button error in button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -17,11 +17,11 @@ const checkButtonStyle = STYLES.includes(buttonStyle)
 ? buttonStyle 
 : STYLES[0];
 
-const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0]
+const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
 return(
     <Link to='/sign-up' className='btn-mobile'>
-        <button className={'btn ${checkButtonStyle} ${checkButtonSize}'}
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
         type={type}
         >
