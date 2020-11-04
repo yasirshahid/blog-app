49d6051d8ff9daa12d2b5915646c866a082420e8
yasirshahid <yasirshahid5@gmail.com>
2020-11-04 19:31:37 +0500

    update code

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import '../../App.css'
+import Footer from '../Footer';
 
-export default function products(){
-    return <h1 className="products">PRODUCTS</h1>
-    
+export default function products() {
+    return (
+        <div>
+            <h1 className="products">PRODUCTS</h1>
+            <Footer/>
+        </div>
+    )
+
 }
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import Footer from '../Footer';
 
 export default function Services(){
-    return <h1 className="services">SERVICES</h1>
+    return (
+        <div>
+            <h1 className="services">SERVICES</h1>
+            <Footer/>
+        </div>
+
+    )
 }
