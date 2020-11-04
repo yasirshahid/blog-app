55ad4c01f0c7938bf5430933f6572f9127d7cf2f
yasirshahid <yasirshahid5@gmail.com>
2020-11-04 17:28:48 +0500

    The End

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -7,7 +7,7 @@ function CardItem(props) {
             <li className="cards__item">
                 <Link className="cards__item__link" to={props.path}>
                     <figure className="cards__item__pic-wrap" data-category={props.label}>
-                        <image
+                        <img
                             className="cards__item__img"
                             src={props.src}
                             alt="Traval Image"
